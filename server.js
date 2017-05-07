a0c0e990baeed1e365395a4171ef88a46cc5e2de
ssnikiforov <stepan001@yandex.ru>
2017-05-07 20:25:52 +0300

    Prepare repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-var express = require('express');
+let express = require('express');
 
-var app = express();
+let app = express();
+const PORT = process.env.PORT || 3000; // 3000 for localhost
+
+// for heroku
+// express middleware, for redirect traffic from openWeatherMap`s http to https middleware service
+app.use(function (request, response, next) {
+  if (request.headers['x-forwarded-proto'] === 'http') {
+      next();
+  } else {
+      response.redirect('http://' + request.hostname + request.url);
+  }
+};
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-	console.log('Server is started on port 3000');
+app.listen(PORT, function () {
+    console.log('Server is started on port ' + port);
 });
